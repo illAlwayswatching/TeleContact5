98c2f6f0c7715bdece46c54a39e9cf335ac40fd6
illAlwayswatching <113437802+illAlwayswatching@users.noreply.github.com>
2024-05-23 15:02:42 +0800

    Update Contact.kt

--- app/src/main/java/Contact.kt
+++ app/src/main/java/Contact.kt
@@ -1,2 +1,13 @@
-class Contact {
-}
+// Contact.kt
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts")
+data class Contact(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val telephone: String,
+    val group: String
+)
